1703c0afd1627135e307ff9c861cb5daaca34a72
mujeebijaz <mujeeb_ijaz@hotmail.com>
2021-09-15 00:37:30 +0500

    Second commit

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//Connecting Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
+app.get('/', (req, res) => res.send('API is running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 
 app.listen(PORT, () => {
     console.log('Server is running');
